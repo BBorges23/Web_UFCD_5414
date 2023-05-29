50490d25a01cc3d8432dad61760d6f47cd08d815
BrunoOrike <bborges31@gmail.com>
2023-05-29 23:22:12 +0100

    atualizacao 29/05/2023-2

---
+++ dbrepo.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+
+exports.databaseRepo = function () {
+
+    var dbName = 'database.json';
+    
+    var readDb = function(){
+        var rawData = fs.readFileSync(dbName);
+        return JSON.parse(rawData)
+    };
+
+
+    return {
+        /*
+        update: function(person){     
+            var dataJson = readDb();
+
+            var personToUpdate = dataJson.people.find(x => x.Id === person.Id);
+
+            personToUpdate.FirstName = person.FirstName;
+            personToUpdate.LastName = person.LastName;
+            personToUpdate.Age = person.Age;
+            personToUpdate.Email = person.Email;
+            
+            fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
+
+            return personToUpdate
+        },
+        */
+        /*
+        insert: function(person){     
+            var dataJson = readDb();
+
+            let nextId = 1;
+            for (const person of dataJson.people) {
+                if(nextId <= person.Id){
+                    nextId = person.Id
+                }
+            }
+            nextId++;
+            person.Id = nextId;
+            
+            dataJson.people.push(person);            
+            fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
+            return person
+        },
+        */
+        list: function(queryParams){
+            return readDb().dog.filter(a=>a.category==queryParams.category)
+        },
+        /*
+        delete: function(id){
+            var dataJson = readDb();
+            dataJson.people = dataJson.people.filter(x => x.Id != id);
+            
+            fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
+
+            return true;
+        },
+        */
+       /* listSampleData: function(onResponse){
+            fs.readFile('product_list.json', 'utf8', onResponse);
+        }
+    
+    */
+    }
+
+};
--- index.js
+++ index.js
@@ -1,2 +1,51 @@
+const express = require('express')
+var cors = require('cors')
+var app = express()
+var dbRepo = require('./dbrepo').databaseRepo();
+
+const bodyParser = require('body-parser');
+const port = 3000
+
+app.use(cors())
+app.use(bodyParser.json());
+
+// app.post('/addPerson', (req, res) => {
+//   let data = req.body;
+//   let insertedPerson = dbRepo.insert(data)
+//   res.send(insertedPerson);
+// })
+
+// app.post('/updatePerson', (req, res) => {
+//   let data = req.body;
+//   let updated = dbRepo.update(data);
+//   res.send(updated);
+// })
+
+// app.post('/deletePerson', (req, res) => {
+//     let id = req.body.id;
+
+//     var result = dbRepo.delete(id)
+//     res.send(result ? 'OK' : 'NOK');
+//     // res.send('api request' + id);
+// })
+
+app.get('/animal', (req, res) => {
+  res.send(dbRepo.list(req.query))
+  // res.send(req.query)
+})
+
+
+// app.get('/sample', (req, res) => {
+//   dbRepo.listSampleData(function(err, data){
+//     var dictJson = JSON.parse(data);
+//     res.send(dictJson)
+//   })
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
 
 
