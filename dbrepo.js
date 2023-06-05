ca184471609c2193cb3df40ee233af0269616c97
Miguel Jesus <miguelclaudiojesus@gmail.com>
2023-06-05 17:36:14 +0100

    update 05/06/2023

--- dbrepo.js
+++ dbrepo.js
@@ -3,126 +3,77 @@ const fs = require('fs')
 exports.databaseRepo = function () {
 
     var dbName = 'database.json';
-
-    var readDb = function () {
+    
+    var readDb = function(){
         var rawData = fs.readFileSync(dbName);
         return JSON.parse(rawData)
     };
 
 
     return {
-        /*
-        update: function(person){     
-            var dataJson = readDb();
-
-            var personToUpdate = dataJson.people.find(x => x.Id === person.Id);
-
-            personToUpdate.FirstName = person.FirstName;
-            personToUpdate.LastName = person.LastName;
-            personToUpdate.Age = person.Age;
-            personToUpdate.Email = person.Email;
-            
-            fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
-
-            return personToUpdate
+       
+        list: function(queryParams, arrayName){
+            return readDb()[arrayName].filter(a => a.category == queryParams.category);
         },
-        */
+        
 
-        /*         insert: function (animal, category, product) {
-                    var dataJson = readDb();
-
-                    let nextId = 1;
-                    for (const p of dataJson[animal][category]) {
-                        if (nextId <= p.Id) {
-                            nextId = p.Id
-                        }
-                    }
-                    nextId++;
-                    product.Id = nextId;
-
-                    dataJson[animal][category].push(product);
-                    fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
-                    return product;
-                },
-         */
         delete: function (animalType, id) {
-        var dataJson = readDb();
-
-        if (dataJson.hasOwnProperty(animalType)) {
-            var animalArray = dataJson[animalType];
-            for (var i = 0; i < animalArray.length; i++) {
-            if (animalArray[i].id === id) {
-                animalArray.splice(i, 1);
-                fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
-                return true;
-            }
-            }
-        }
-
-        return false;
-        },
-
-        insert: function (product) {
             var dataJson = readDb();
-
-            // Find the array for the specified animal
-            var animal = product.animal;
-            if (!dataJson[animal]) {
-                // If the array doesn't exist, create it
-                dataJson[animal] = [];
-            }
-
-            // Generate the next id for the product
-            var animalArray = dataJson[animal];
-            var maxId = 0;
-            for (const p of animalArray) {
-                if (p.id > maxId) {
-                    maxId = p.id;
+    
+            if (dataJson.hasOwnProperty(animalType)) {
+                var animalArray = dataJson[animalType];
+                for (var i = 0; i < animalArray.length; i++) {
+                if (animalArray[i].id === id) {
+                    animalArray.splice(i, 1);
+                    fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
+                    return true;
+                }
                 }
             }
-            var nextId = maxId + 1;
-            product.id = nextId;
+            return false;
+            },
 
-            // Reorder the properties of the product object
-            var orderedProduct = {
-                id: product.id,
-                name: product.name,
-                brand: product.brand,
-                category: product.category,
-                description: product.description,
-                price: product.price,
-                stock: product.stock,
-                image: product.image
-            };
-
-            // Add the product to the array
-            animalArray.push(orderedProduct);
-
-            // Write the updated data back to the file
-            fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
-
-            return product;
-        },
-
-        list: function (queryParams, arrayName) {
-            return readDb()[arrayName].filter(a => a.category == queryParams.category);
-        }
-
-        /*
-        delete: function(id){
-            var dataJson = readDb();
-            dataJson.people = dataJson.people.filter(x => x.Id != id);
-            
-            fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
-
-            return true;
-        },
-        */
-        /* listSampleData: function(onResponse){
-            fs.readFile('product_list.json', 'utf8', onResponse);
-        }
+            insert: function (product) {
+                var dataJson = readDb();
     
-    */
+                // Find the array for the specified animal
+                var animal = product.animal;
+                if (!dataJson[animal]) {
+                    // If the array doesn't exist, create it
+                    dataJson[animal] = [];
+                }
+    
+                // Generate the next id for the product
+                var animalArray = dataJson[animal];
+                var maxId = 0;
+                for (const p of animalArray) {
+                    if (p.id > maxId) {
+                        maxId = p.id;
+                    }
+                }
+                var nextId = maxId + 1;
+                product.id = nextId;
+    
+                // Reorder the properties of the product object
+                var orderedProduct = {
+                    id: product.id,
+                    name: product.name,
+                    brand: product.brand,
+                    category: product.category,
+                    description: product.description,
+                    price: product.price,
+                    stock: product.stock,
+                    image: product.image
+                };
+    
+                // Add the product to the array
+                animalArray.push(orderedProduct);
+    
+                // Write the updated data back to the file
+                fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
+    
+                return product;
+            }
     }
 
 };
--- index.js
+++ index.js
@@ -9,20 +9,10 @@ const port = 3000
 app.use(cors())
 app.use(bodyParser.json());
 
-
-// app.post('/updatePerson', (req, res) => {
-//   let data = req.body;
-//   let updated = dbRepo.update(data);
-//   res.send(updated);
-// })
-
-// app.post('/deletePerson', (req, res) => {
-//     let id = req.body.id;
-
-//     var result = dbRepo.delete(id)
-//     res.send(result ? 'OK' : 'NOK');
-//     // res.send('api request' + id);
-// })
+app.get('/animal/:category', (req, res) => {
+  const { category } = req.params;
+  res.send(dbRepo.list(req.query, category));
+});
 
 app.post('/deleteProduto', (req, res) => {
   let id = req.body.id;
@@ -32,24 +22,11 @@ app.post('/deleteProduto', (req, res) => {
   res.send(result ? 'OK' : 'NOK');
 });
 
-app.get('/animal/:category', (req, res) => {
-  const { category } = req.params;
-  res.send(dbRepo.list(req.query, category));
-});
-
 app.post('/addProduto', (req, res) => {
   let data = req.body;
   let insertedProduct = dbRepo.insert(data);
   res.send(insertedProduct);
 });
-
-
-// app.get('/sample', (req, res) => {
-//   dbRepo.listSampleData(function(err, data){
-//     var dictJson = JSON.parse(data);
-//     res.send(dictJson)
-//   })
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
