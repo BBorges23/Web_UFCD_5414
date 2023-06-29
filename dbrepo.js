e895d078cdd78c3140fa9415b3113a9b7ab48c93
RUI GAMEIRO <ruigameiro11@gmail.com>
2023-06-29 23:39:12 +0100

    comentários 290623

--- dbrepo.js
+++ dbrepo.js
@@ -13,6 +13,10 @@ exports.databaseRepo = function () {
     return {
 
         list: function (queryParams, arrayName) {
+            
+          /* É aplicado um filtro no array para selecionar apenas os elementos 
+          cuja propriedade `category` seja igual a `queryParams.category`
+          O resultado filtrado é retornado como resultado da função. */
             return readDb()[arrayName].filter(a => a.category == queryParams.category);
         },
 
@@ -20,41 +24,52 @@ exports.databaseRepo = function () {
         delete: function (animalType, id) {
             var dataJson = readDb();
 
+            // Verifica se `dataJson` possui a propriedade `animalType`.
             if (dataJson.hasOwnProperty(animalType)) {
+                // Obtém o array correspondente a `animalType` e o armazena na variável `animalArray`.
                 var animalArray = dataJson[animalType];
                 for (var i = 0; i < animalArray.length; i++) {
+                     // Verifica se o valor da propriedade `id` é igual ao `id` fornecido.
                     if (animalArray[i].id === id) {
+                        // Se encontrou o elemento com o `id` correspondente, o remove do array usando `splice`.
                         animalArray.splice(i, 1);
+                        // Atualiza o banco de dados escrevendo os dados de volta no arquivo
                         fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
+                        // Retorna `true` para indicar que a exclusão foi bem-sucedida.
                         return true;
                     }
                 }
             }
+            // Se o `animalType` não existe no banco de dados ou o `id` não corresponde a nenhum elemento, retorna `false`.
             return false;
         },
 
         insert: function (product) {
             var dataJson = readDb();
 
-            // Find the array for the specified animal
+            // Encontra o array específico para o tipo de animal
             var animal = product.animal;
             if (!dataJson[animal]) {
-                // If the array doesn't exist, create it
+                //Se o array não existir, é criado
                 dataJson[animal] = [];
             }
 
-            // Generate the next id for the product
+            // Obtém o array de produtos correspondente ao tipo de animal
             var animalArray = dataJson[animal];
             var maxId = 0;
+
+            // Percorre cada produto no array para encontrar o ID máximo
             for (const p of animalArray) {
                 if (p.id > maxId) {
                     maxId = p.id;
                 }
             }
+            // Gera o próximo ID adicionando 1 ao ID máximo encontrado
             var nextId = maxId + 1;
+            // Atualiza o ID do produto a ser inserido com o próximo ID
             product.id = nextId;
 
-            // Reorder the properties of the product object
+            // Reordena as propriedades do objeto `product` para uma ordem específica.
             var orderedProduct = {
                 id: product.id,
                 name: product.name,
@@ -66,22 +81,24 @@ exports.databaseRepo = function () {
                 image: product.image
             };
 
-            // Add the product to the array
+             // Adiciona o produto ao array `animalArray`.
             animalArray.push(orderedProduct);
 
-            // Write the updated data back to the file
+            // Escreve os dados atualizados de volta no arquivo
             fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
 
+            // Retorna o objeto `product` com o ID atualizado.
             return product;
         },
         update: function (animalType, id, newData) {
             var dataJson = readDb();
 
             if (dataJson.hasOwnProperty(animalType)) {
+                // Obtém o array de produtos correspondente ao tipo de animal
                 var animalArray = dataJson[animalType];
                 for (var i = 0; i < animalArray.length; i++) {
                     if (animalArray[i].id === id) {
-                        // Update the properties of the product with the new data
+                        // Atualiza as propriedades do produto com os novos dados
                         animalArray[i].name = newData.name;
                         animalArray[i].brand = newData.brand;
                         animalArray[i].description = newData.description;
@@ -89,26 +106,14 @@ exports.databaseRepo = function () {
                         animalArray[i].stock = newData.stock;
                         animalArray[i].image = newData.image;
 
-                        // Write the updated data back to the file
+                        // Escreve os dados atualizados de volta no arquivo
                         fs.writeFileSync(dbName, JSON.stringify(dataJson, null, 2));
 
-                        return animalArray[i]; // Return the updated product
+                        return animalArray[i]; // Retorna o produto atualizado
                     }
                 }
             }
-            return null; // Product not found
-        },
-        login: function (username, password) {
-            var dataJson = readDb();
-            var users = dataJson["users"];
-        
-            for (let index = 0; index < users.length; index++) {
-                if (username === users[index].username && password === users[index].password) {
-                    return users[index].username;
-                }
-            }
-        
-            return null; // Retorna null se as credenciais forem inválidas
+            return null; // Produto não encontrado
         }
         
     }
--- index.js
+++ index.js
@@ -12,39 +12,44 @@ app.use(bodyParser.json());
 
 
 app.get('/animal/:category', (req, res) => {
-  const { category } = req.params;
-  res.send(dbRepo.list(req.query, category));
-});
-
-app.post('/deleteProduto', (req, res) => {
-  let id = req.body.id;
-  let animalType = req.body.animalType; // Adicione esta linha para obter o tipo de animal
-
-  var result = dbRepo.delete(animalType, id); // Modifique a chamada para a função delete
-  res.send(result ? 'OK' : 'NOK');
+  const { category } = req.params; // Obtém o parâmetro 'category' da URL
+  // Chama o método 'list' do objeto 'dbRepo' passando os parâmetros da consulta e a categoria
+  res.send(dbRepo.list(req.query, category));   
 });
 
 app.post('/addProduto', (req, res) => {
-  let data = req.body;
+  let data = req.body; // Obtém os dados do corpo da requisição
+  // Chama o método 'insert' do objeto 'dbRepo' passando os dados do produto
   let insertedProduct = dbRepo.insert(data);
-  res.send(insertedProduct);
+  res.send(insertedProduct); // Envia o produto inserido como resposta para o cliente
+});
+
+app.post('/deleteProduto', (req, res) => {
+  let id = req.body.id; // Obtém o ID do produto a ser apagado do corpo da requisição
+  let animalType = req.body.animalType; // Adicione esta linha para obter o tipo de animal
+
+  var result = dbRepo.delete(animalType, id); // Modifica a chamada para a função delete
+  // Envia 'OK' se a operação for bem-sucedida, caso contrário, envia 'NOK'
+  res.send(result ? 'OK' : 'NOK');
+});
+
+app.post('/updateProduto', (req, res) => {
+  let id = req.body.id; // Obtém o ID do produto a ser atualizado do corpo da requisição
+  let newData = req.body.data; // Obtém o tipo de animal do corpo da requisição
+  let animal = req.body.animal; // Obtém o tipo de animal do corpo da requisição
+  let updatedProduct = dbRepo.update(animal, id, newData); // Chama a função update do dbRepo para atualizar o produto
+
+  if (updatedProduct) {
+    // Se o produto foi atualizado com sucesso, envia o produto atualizado como resposta
+    res.send(updatedProduct);
+  } else {
+    // Caso contrário, envia uma resposta com status 404 (Not Found) e uma mensagem indicando que o produto não foi encontrado
+    res.status(404).send('Produto nÃo encontrado');
+  }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-app.post('/updateProduto', (req, res) => {
-  let id = req.body.id;
-  let newData = req.body.data;
-  let animal = req.body.animal;
-  let updatedProduct = dbRepo.update(animal, id, newData);
-
-  if (updatedProduct) {
-    res.send(updatedProduct);
-  } else {
-    res.status(404).send('Produto nÃo encontrado');
-  }
-});
 
 
